e64b927c254ced17c9690b21de39316a131e74c2
Ankit Sharma <ankitsharma290101@gmail.com>
2023-12-04 22:38:41 +0530

    using Link and Image (Next config)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Header from "@/components/Header";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Drop box</div>
+      Main
     </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,24 @@
 import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex justify-around">
-      <p>Dropbox</p>
+    <header className="flex justify-around">
+      <Link href="/">
+        <div>
+          <Image
+            src="https://www.shareicon.net/download/2016/07/13/606936_dropbox_2048x2048.png"
+            alt="logo"
+            // className="invert"
+            height={50}
+            width={50}
+          />
+        </div>
+        <h1 className="text-2xl font-bold">Dropbox</h1>
+      </Link>
       <UserButton afterSignOutUrl="/" />
-    </div>
+    </header>
   );
 };
 export default Header;
