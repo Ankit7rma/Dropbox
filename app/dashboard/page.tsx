f43bfdc3a53b4dd3698d69566cabc6de5b492a35
Ankit Sharma <ankitsharma290101@gmail.com>
2023-12-10 17:58:09 +0530

    Styling Css Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,6 @@ import DropZone from "@/components/DropZone";
 function Dashboard() {
   return (
     <div className="text-red-300 dark:text-blue-800 flex flex-col justify-center items-center">
-      Dashboard
       <DropZone />
     </div>
   );
--- components/DropZone.tsx
+++ components/DropZone.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { cn } from "@/lib/utils";
 import DropzoneComponent from "react-dropzone";
 
 const DropZone = () => {
@@ -21,8 +22,16 @@ const DropZone = () => {
             fileRejections.length > 0 && fileRejections[0].file.size > maxsize;
 
           return (
-            <section>
-              <div {...getRootProps()}>
+            <section className="m-4">
+              <div
+                {...getRootProps()}
+                className={cn(
+                  "w-full h-52 flex justify-center items-center p-5 border border-dashed rounded-lg",
+                  isDragActive
+                    ? "bg-blue-950"
+                    : "bg-slate dark:bg-slate-500 text-slate-300"
+                )}
+              >
                 <input {...getInputProps()} />
                 {!isDragActive && "Click here or drag files to drop"}
                 {isDragActive && !isDragReject && "Drop to upload this file"}
