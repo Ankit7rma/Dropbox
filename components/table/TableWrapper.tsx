2c6c5a08d6f0b7040d5cd2e1e9adb8d93a8e3441
Ankit Sharma <ankitsharma290101@gmail.com>
2023-12-15 16:44:02 +0530

    timestamp

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -1,12 +1,14 @@
 import { FileType } from "@/typings";
 import React from "react";
 import { Button } from "../ui/button";
+import { DataTable } from "./Table";
+import { columns } from "./columns";
 
 function TableWrapper({ skeletonFiles }: { skeletonFiles: FileType[] }) {
   return (
     <div>
       <Button>Sort By...</Button>
-      {/* <DataTable columns={columns} data={initialFiles} /> */}
+      <DataTable columns={columns} data={skeletonFiles} />
     </div>
   );
 }
